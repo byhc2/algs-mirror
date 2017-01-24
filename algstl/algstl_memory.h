78c26368684351b12a1c44cd57bf29317ac01350
dsh <dshbusiness@gmail.com>
2017-01-25 01:29:56 +0800

    update

--- algstl/algstl_memory.h
+++ algstl/algstl_memory.h
@@ -1,21 +1,23 @@
 #ifndef __ALGSTL_MEMORY_H__
 #define __ALGSTL_MEMORY_H__
 
+#include <new>
+
 namespace algstl
 {
 
-template <typename _Tp>
+template <typename Tp>
 class allocator
 {
 
 public:
     typedef size_t      size_type;
     typedef ptrdiff_t   difference_type;
-    typedef _Tp*        pointer;
-    typedef const _Tp*  const_pointer;
-    typedef _Tp&        reference;
-    typedef const _Tp&  const_reference;
-    typedef _Tp         value_type;
+    typedef Tp*        pointer;
+    typedef const Tp*  const_pointer;
+    typedef Tp&        reference;
+    typedef const Tp&  const_reference;
+    typedef Tp         value_type;
 
     //这里需要单独声明一个struct
     //使容器层在使用这个allocator的时候
@@ -23,14 +25,14 @@ public:
     //比如list容器，数据本身是存放在list_node中的
     //所以在知道了数据本身的allocator之后
     //可以通过rebind来得到list_node的allocator
-    template <typename _Tp1> struct rebind
+    template <typename Tp1> struct rebind
     {
-        typedef allocator<_Tp1> other;
+        typedef allocator<Tp1> other;
     };
 
     allocator() = default;
     allocator(const allocator &) {}
-    template <typename _Tp1> allocator(const allocator<_Tp1> &) {}
+    template <typename Tp1> allocator(const allocator<Tp1> &) {}
     ~allocator() {}
 
     pointer address(reference x) const
@@ -43,19 +45,33 @@ public:
         return &x;
     }
 
-    _Tp *allocate(size_type n)
+    Tp *allocate(size_type n)
     {
         if (n == 0)
         {
             return 0;
         }
 
-        void *p = malloc(n * sizeof(_Tp));
+        void *p = malloc(n * sizeof(Tp));
         if (p)
         {
             return p;
         }
-        throw bad_alloc;
+        throw bad_alloc();
+    }
+
+    void deallocate(pointer p, size_type n)
+    {
+        //因为分配的时候调用的是malloc
+        //操作系统已经记录了n的信息
+        //所以这里不用再使用n
+        free(p);
+    }
+
+    void construct(pointer p, const Tp &val)
+    {
+        //new (p) Tp(val);
+        new (p) (val); //FIXME 试试这样行不行
     }
 };
 
