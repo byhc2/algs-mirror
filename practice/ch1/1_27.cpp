973bc86bc188a22dc59ac40202f4c88c3c9ace50
dsh <dshbusiness@gmail.com>
2017-08-03 17:37:10 +0800

    update

---
+++ practice/ch1/1_27.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "algs_type.h"
+#include "algstl_array.h"
+#include "algsnum.h"
+
+using namespace std;
+using namespace algs;
+using namespace algstl;
+
+Ulong recursive_counter = 0;
+
+Array<Array<Double>> preload;
+
+//经过观察，n < k时，递归调用次数为 2 ^ (n + 2) - 1次
+//n > k时，结果可能是一个k + 1阶的等差数列
+//所以binomial(100, 50, p)差不多是一个51阶等差数列，调用次数大约为100 ^ 51次
+Double binomial(Int n, Int k, Double p)
+{
+    ++recursive_counter;
+    if (n == 0 && k == 0)
+        return 1.0;
+    if (n < 0 || k < 0)
+        return 0.0;
+    return (1.0 - p) * binomial(n - 1, k, p) + p * binomial(n - 1, k - 1, p);
+}
+
+int main(int argc, char *argv[])
+{
+    binomial(Num::parseInt(argv[1]), Num::parseInt(argv[2]), 0.5);
+    cout << recursive_counter << endl;
+
+    return 0;
+}
