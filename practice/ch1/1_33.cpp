c3cb1c76f488594fdc6866fc151c752782d95679
mythyear <dshbusiness@gmail.com>
2017-08-22 09:01:52 +0800

    update

---
+++ practice/ch1/1_33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <initializer_list>
+
+class Matrix
+{
+    //向量
+    class Vector
+    {
+    public:
+        Vector(const initializer_list<Double> &lst)
+        {
+        }
+    };
+};
