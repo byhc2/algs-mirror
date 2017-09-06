b302e2078aec4526fab6aa270525c35dc15065c4
dsh <dshbusiness@gmail.com>
2017-09-06 09:26:10 +0800

    update

--- practice/ch1/1_36.cpp
+++ practice/ch1/1_36.cpp
@@ -9,6 +9,30 @@ using namespace std;
 using namespace algstl;
 using namespace algs;
 
+template<typename T>
+void shuffle(algstl::Array<T> &a)
+{
+    for (auto i = 0u; i < a.size(); ++i)
+    {
+        auto j = i + AlgsRandom::uniform((Int)a.size() - i);
+        Double tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
+
+template<typename T>
+void shuffle2(algstl::Array<T> &a)
+{
+    for (auto i = 0u; i < a.size(); ++i)
+    {
+        auto j = AlgsRandom::uniform((Int)a.size());
+        Double tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     assert(argc == 3);
@@ -25,7 +49,8 @@ int main(int argc, char *argv[])
             a[j] = j;
         }
 
-        AlgsRandom::shuffle(a);
+        //AlgsRandom::shuffle(a);
+        shuffle2(a);
 
         for (auto j = 0; j < m; ++j)
         {
