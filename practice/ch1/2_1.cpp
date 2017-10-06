e6c573e7a7a034c0a88dcdbb17dc2bd867d38ece
mythyear <dshbusiness@gmail.com>
2017-10-07 00:24:16 +0800

    update

--- practice/ch1/2_1.cpp
+++ practice/ch1/2_1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "geometry.h"
 #include "algsnum.h"
+#include "algsrandom.h"
 
 using namespace std;
 using namespace algs;
@@ -8,8 +9,13 @@ using namespace algs;
 int main(int argc, char *argv[])
 {
     assert(argc == 2);
+    AlgsRandom::initialize();
 
     Int n = Num::parseInt(argv[1]);
 
-    while
+    for (auto i = 0; i < n; ++i)
+    {
+        auto x = AlgsRandom::random();
+        auto y = AlgsRandom::random();
+    }
 }
