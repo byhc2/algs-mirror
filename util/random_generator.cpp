ee9fff8c49c641ca0d7bf0460b7cd8b82eb98cef
dsh <dshbusiness@gmail.com>
2018-01-04 01:46:03 +0800

    update

--- util/random_generator.cpp
+++ util/random_generator.cpp
@@ -1,6 +1,7 @@
+#include <libintl.h>
 #include <unistd.h>
+#include <cstdio>
 #include "algsrandom.h"
-
 
 void usage(int status)
 {
@@ -26,9 +27,85 @@ static struct option long_options[] = {
     {"help", required_argument, 0, 'h'},
     {0, 0, 0, 0},
 };
+const char optstr[] = "m:M:t:h";
 
-int type;
-int imin, imax;
+enum GenType
+{
+    UNDEFINED_TYPE,
+    INT_TYPE,
+    FLOAT_TYPE,
+};
+struct Config
+{
+    Config() : has_min(0), has_max(0), type(UNDEFINED_TYPE)
+    {}
+    int has_min;
+    int has_max;
+    union min
+    {
+        int i;
+        double d;
+    };
+    union max
+    {
+        int i;
+        double d;
+    };
+    GenType type;
+} config;
+
+int parseOpt(int argc, char *argv[])
+{
+    int c;
+    int optidx;
+    while ((c = getopt_long(argc, argv, optstr, option, &optidx)) != -1)
+    {
+        switch (c)
+        {
+        case 'm':
+            config.has_min = 1;
+            switch (config.type)
+            {
+            case INT_TYPE:
+                config.min.i = atoi(optarg);
+                break;
+            case FLOAT_TYPE:
+                config.min.d = atof(optarg);
+                break;
+            default:
+                break;
+            };
+            break;
+        case 'M':
+            config.has_max = 1;
+            switch (config.type)
+            {
+            case INT_TYPE:
+                config.max.i = atoi(optarg);
+                break;
+            case FLOAT_TYPE:
+                config.max.d = atof(optarg);
+                break;
+            default:
+                break;
+            };
+            break;
+        case 't':
+            if(string(optarg)=="i")
+            {
+                config.type=INT_TYPE;
+            }
+            else if(string(optarg)=="d")
+            {
+                config.type=FLOAT_TYPE;
+            }
+            if(config.type==UNDEFINED_TYPE)
+            {
+            }
+            break;
+        };
+    }
+}
 
 int main(int argc, char *argv[])
 {
